91d67ced5af4e9935863cc9a161de486c53194d7
AlphaAnimal <h.h.redondo@gmail.com>
2026-02-10 16:13:58 +0100

    switched to id token

--- src/services/flaskService.js
+++ src/services/flaskService.js
@@ -21,7 +21,7 @@ class FlaskService {
   async getAuthHeaders() {
     try {
       const session = await fetchAuthSession();
-      const token = session.tokens?.accessToken?.toString();
+      const token = session.tokens?.idToken?.toString();
       
       return {
         'Content-Type': 'application/json',
