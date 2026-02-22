a80cda67fa4637cf6f3e55430bf608d5b974f368
AlphaAnimal <h.h.redondo@gmail.com>
2026-02-22 13:52:37 +0100

    upload S3 storage name to SSM

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,15 +1,27 @@
 import { defineBackend } from "@aws-amplify/backend";
+import { Stack } from "aws-cdk-lib";
+import * as ssm from "aws-cdk-lib/aws-ssm";
 import { auth } from "./auth/resource";
+import { customFunction } from "./custom-function/resource";
 import { data } from "./data/resource";
 import { storage } from "./storage/resource";
-import { customFunction } from "./custom-function/resource";
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
   storage,
   customFunction,
+});
+
+const customStack = backend.createStack("custom-resources");
+const stackName = Stack.of(customStack).stackName;
+
+const mediaBucket = backend.storage.resources.bucket;
+
+new ssm.StringParameter(customStack, "MediaBucketNameParam", {
+  parameterName: `/${stackName}/MediaBucketName`,
+  stringValue: mediaBucket.bucketName,
 });
